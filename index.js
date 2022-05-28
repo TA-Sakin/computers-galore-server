eebf697132fb8b87237cdc1c7c4c4d311740de1a
TA-Sakin <tasakin98@gmail.com>
2022-05-28 16:32:19 +0600

    data loaded

---
+++ index.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdum6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollection = client.db("computersGalore").collection("tools");
+
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const result = await toolsCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Computers Galore");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
