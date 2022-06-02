a51e4a2fa60f5427d54e3dbb00a90c978cdb010b
TA-Sakin <tasakin98@gmail.com>
2022-06-02 23:43:20 +0600

    cors issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-app.use(cors());
+app.use(cors({origin: "https://computer-galore.web.app/signup"}));
 app.use(express.json());
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
