cc46f2327a56120f1fa25cee744def2e27c7fe2d
TA-Sakin <tasakin98@gmail.com>
2022-06-03 15:41:18 +0600

    cors issue

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const corsConfig = {
-  origin: "https://computer-galore.web.app"
-};
-app.use(cors(corsConfig));
+// const corsConfig = {
+//   origin: "https://computer-galore.web.app"
+// };
+app.use(cors({ origin: "https://computer-galore.web.app" }));
 // app.options('*',corsConfig)
 // app.use(cors());
 app.use(express.json());
